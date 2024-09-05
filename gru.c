e285e589b2e702d24387ea13773655b00fd7e6ad
HabiburRahman1 <habib.usa2014@gmail.com>
2024-09-05 20:31:27 +0900

    Update the code

--- gru.c
+++ gru.c
@@ -2,37 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
-#define INPUT_SIZE 6  // Number of features: rw_rat, is_seq, chunk_w, chunk_r, etc.
+#define INPUT_SIZE 6   // Number of features: rw_rat, is_seq, chunk_w, chunk_r, etc.
 #define HIDDEN_SIZE 10 // Number of units in the GRU's hidden state
 #define OUTPUT_SIZE 1  // We are predicting life_t, which is a single value
 #define LEARNING_RATE 0.001
 #define EPOCHS 1000
 
 // Activation functions
-double sigmoid(double x) {
+double sigmoid(double x)
+{
     return 1.0 / (1.0 + exp(-x));
 }
 
-double dsigmoid(double x) {
+double dsigmoid(double x)
+{
     return x * (1.0 - x);
 }
 
-double tanh_activation(double x) {
+double tanh_activation(double x)
+{
     return tanh(x);
 }
 
-double dtanh_activation(double x) {
+double dtanh_activation(double x)
+{
     return 1.0 - x * x;
 }
 
 // Random initialization
-double random_weight() {
+double random_weight()
+{
     return (double)rand() / RAND_MAX * 2.0 - 1.0; // Random values between -1 and 1
 }
 
 // Struct to represent the GRU model
-typedef struct {
+typedef struct
+{
     double Wz[HIDDEN_SIZE][INPUT_SIZE];  // Update gate weights
     double Uz[HIDDEN_SIZE][HIDDEN_SIZE]; // Recurrent update gate weights
     double Wr[HIDDEN_SIZE][INPUT_SIZE];  // Reset gate weights
@@ -48,14 +55,18 @@ typedef struct {
 } GRU;
 
 // Initialize the GRU weights randomly
-void initialize_gru(GRU *gru) {
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
-        for (int j = 0; j < INPUT_SIZE; j++) {
+void initialize_gru(GRU *gru)
+{
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
+        for (int j = 0; j < INPUT_SIZE; j++)
+        {
             gru->Wz[i][j] = random_weight();
             gru->Wr[i][j] = random_weight();
             gru->Wh[i][j] = random_weight();
         }
-        for (int j = 0; j < HIDDEN_SIZE; j++) {
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+        {
             gru->Uz[i][j] = random_weight();
             gru->Ur[i][j] = random_weight();
             gru->Uh[i][j] = random_weight();
@@ -68,18 +79,22 @@ void initialize_gru(GRU *gru) {
 }
 
 // Forward pass through the GRU cell
-void gru_forward(GRU *gru, double *input, double *output) {
+void gru_forward(GRU *gru, double *input, double *output)
+{
     double z[HIDDEN_SIZE], r[HIDDEN_SIZE], h_tilde[HIDDEN_SIZE], h_next[HIDDEN_SIZE];
 
     // Compute update and reset gates
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
         double z_sum = gru->bz[i];
         double r_sum = gru->br[i];
-        for (int j = 0; j < INPUT_SIZE; j++) {
+        for (int j = 0; j < INPUT_SIZE; j++)
+        {
             z_sum += gru->Wz[i][j] * input[j];
             r_sum += gru->Wr[i][j] * input[j];
         }
-        for (int j = 0; j < HIDDEN_SIZE; j++) {
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+        {
             z_sum += gru->Uz[i][j] * gru->hidden_state[j];
             r_sum += gru->Ur[i][j] * gru->hidden_state[j];
         }
@@ -88,40 +103,49 @@ void gru_forward(GRU *gru, double *input, double *output) {
     }
 
     // Compute candidate hidden state
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
         double h_sum = gru->bh[i];
-        for (int j = 0; j < INPUT_SIZE; j++) {
+        for (int j = 0; j < INPUT_SIZE; j++)
+        {
             h_sum += gru->Wh[i][j] * input[j];
         }
-        for (int j = 0; j < HIDDEN_SIZE; j++) {
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+        {
             h_sum += gru->Uh[i][j] * gru->hidden_state[j] * r[i];
         }
         h_tilde[i] = tanh_activation(h_sum);
     }
 
     // Compute next hidden state
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
         h_next[i] = (1 - z[i]) * gru->hidden_state[i] + z[i] * h_tilde[i];
     }
 
     // Update hidden state
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
         gru->hidden_state[i] = h_next[i];
     }
 
     // Output layer (linear)
     output[0] = 0.0;
-    for (int i = 0; i < HIDDEN_SIZE; i++) {
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+    {
         output[0] += h_next[i]; // Simple linear combination
     }
 }
 
 // Training function
-void train_gru(GRU *gru, double input[][INPUT_SIZE], double target[], int dataset_size) {
-    for (int epoch = 0; epoch < EPOCHS; epoch++) {
+void train_gru(GRU *gru, double input[][INPUT_SIZE], double target[], int dataset_size)
+{
+    for (int epoch = 0; epoch < EPOCHS; epoch++)
+    {
         double total_loss = 0.0;
 
-        for (int i = 0; i < dataset_size; i++) {
+        for (int i = 0; i < dataset_size; i++)
+        {
             double output[OUTPUT_SIZE];
             gru_forward(gru, input[i], output);
 
@@ -139,7 +163,35 @@ void train_gru(GRU *gru, double input[][INPUT_SIZE], double target[], int datase
     }
 }
 
-int main() {
+int main()
+{
+
+    // Read dataset from .log file
+    FILE *file = fopen("Data/FIO_train.log", "r");
+    if (file == NULL)
+    {
+        printf("Error: Could not open file\n");
+        return 1;
+    }
+
+    // Read the dataset and print first line to check
+    char line[256];
+    // input_data size will be the number of lines in the file
+    
+    while (fgets(line, sizeof(line), file))
+    {
+        // separate the line by spaces
+        char *token = strtok(line, " ");
+        while (token != NULL)
+        {
+            // Process the word (token) here
+            printf("Token: %s\n", token);
+
+            // Get the next token
+            token = strtok(NULL, " ");
+        }
+    }
+
     srand(time(NULL));
 
     // Define a GRU model
@@ -151,14 +203,13 @@ int main() {
         {3.0, 1.0, 0.0, 0.0, 1.0, 1.0}, // rw_rat, is_seq, chunk_w, chunk_r, etc.
         {5.0, 1.0, 2.0, 1.0, 1.0, 0.0},
         {7.0, 1.0, 1.0, 1.0, 0.0, 1.0},
-        {9.0, 0.0, 0.0, 0.0, 0.0, 1.0}
-    };
+        {9.0, 0.0, 0.0, 0.0, 0.0, 1.0}};
 
     // Sample target data (the life_t we want to predict)
     double target_data[4] = {14.0, 13.0, 12.0, 11.0};
 
     // Train the GRU model
-    train_gru(&gru, input_data, target_data, 4);
+    // train_gru(&gru, input_data, target_data, 4);
 
     return 0;
 }
